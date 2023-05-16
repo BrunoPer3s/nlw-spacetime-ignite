6d18938e98c9146a7dc9b0c867958162f2fecb69
brunoper3s <brunoperes7@hotmail.com>
2023-05-16 12:25:38 -0300

    Server: Main Packages config

---
+++ server-space-time/src/app.ts
@@ -0,0 +1,11 @@
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+export const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
+})
